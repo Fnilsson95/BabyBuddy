16182bcddd0fdc99abe737e9262139b71db7d74a
fnilsson <gusnilfral@student.gu.se>
2024-09-13 19:59:24 +0200

    Correct folder structure
    Swapped description within files to the correct files!

---
+++ server/resources/babysitters/babysitterRoutes.js
@@ -0,0 +1,20 @@
+// RESTful/Overall Checklist to follow while setting up our resources.
+
+// 1. What RESTful methods does each resource (each Entity in ER diagram is one resource) need? 
+//     CREATE (POST) -- READ (GET) -- UPDATE (PUT/PATCH) -- DELETE (DELETE)
+// Example: Do we need DELETE method for the whole collection of the resource, or only for a specific id of the resource?
+//          Doesn't make sense to be able to delete the whole collection of Guardians for instance.
+//          We should not have redundant methods that is not needed for a resource. 
+
+// 2. Use nouns for your resources NOT verbs
+// Example: camels, calenders, children
+// NOT /getAllCamels or /createNewCamel
+
+// 3. Use plural nouns 
+// Example: /camels (to clearly indicate this is a collection)
+
+// 4. GET method and Query Parameters should not alter the state.
+//    Example: Should not use GET method in combination with an update/delete method which alters the state of the collection/object.
+//    GET /guardians/delete/{guardianId}
+
+
