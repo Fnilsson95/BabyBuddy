275ee3746af159f4eb0ea21911ba133586d8a7f7
eriklidb <eriklidb@student.chalmers.se>
2024-10-04 02:38:28 +0200

    Setup for earning cards and section

---
+++ client/src/components/Earningcard.vue
@@ -0,0 +1,27 @@
+<template>
+  <BCard>
+    <div
+      style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        gap: 0;
+      "
+    >
+      <p style="font-size: 16px; font-weight: 600; margin: 0">Pending:</p>
+      <p style="font-size: 24px; font-weight: 800; margin: 0">€1000</p>
+    </div>
+  </BCard>
+</template>
+
+<script>
+import { BCard, BCardText } from 'bootstrap-vue-next'
+
+export default {
+  name: 'earning-card'
+}
+</script>
+
+<style></style>
