268ae49a379d0aef7c46256590b4782768b2de68
fnilsson <gusnilfral@student.gu.se>
2024-10-05 18:22:38 +0200

    Create Helper Function to dynamically update booking statuses

--- server/resources/bookings/bookingController.js
+++ server/resources/bookings/bookingController.js
@@ -7,6 +7,27 @@ const Guardian = require("../guardians/guardianModel");
 const Babysitter = require("../babysitters/babysitterModel");
 const Children = require("../children/childModel");
 
+// Helper function to update booking statuses based on the current time
+const updateBookingStatus = async (bookings) => {
+  const currentDate = new Date();
+
+  for (const booking of bookings) {
+    // If the status is "Pending" and startDateTime has passed --> "Expired"
+    if (booking.status === "Pending" && booking.startDateTime < currentDate) {
+      await Booking.findByIdAndUpdate(booking._id, { status: "Expired" });
+      booking.status = "Expired";
+    }
+
+    // If the status is "Confirmed" and endDateTimes has passed --> "Completed"
+    else if (booking.status === "Confirmed" && booking.endDateTime < currentDate) {
+      await Booking.findByIdAndUpdate(booking._id, { status: "Completed" });
+      booking.status = "Completed";
+    }
+  }
+  // Return the updated array of bookings
+  return bookings;
+};
+
 // Create a new booking
 controller.post("/", async (req, res) => {
   try {
