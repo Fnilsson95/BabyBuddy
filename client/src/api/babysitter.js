0977002da6798302446b9cd06b373351f18ebb0c
fnilsson <gusnilfral@student.gu.se>
2024-10-06 23:18:40 +0200

    Connect frontend with backend on signup page

--- client/src/api/babysitter.js
+++ client/src/api/babysitter.js
@@ -16,5 +16,23 @@ export const babysitterAPI = {
       console.error('Error fetching users:', error)
       throw error
     }
+  },
+
+  async createBabysitter(babysitterData) {
+    try {
+      const response = await fetch(`${BASE_URL}/babysitters`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(babysitterData)
+      })
+      if (!response.ok) {
+        throw new Error('Failed to create babysitter')
+      }
+      return await response.json()
+    } catch (error) {
+      console.error('Error creating babysitter: ', error)
+    }
   }
 }
