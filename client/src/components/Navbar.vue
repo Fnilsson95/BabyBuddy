e2262efb12137c0b58fc24d13b1da0c0b8ba5cd0
fnilsson <gusnilfral@student.gu.se>
2024-10-14 12:55:18 +0200

    Correct routing and id handling in Navbar
    Removed id handling which uses /defaultUserId as a parameter

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -19,27 +19,27 @@
                     <BDropdownItem to="/login">Sign Out</BDropdownItem>
                 </BNavItemDropdown>
             </BNavbarNav>
-            <BAvatar to="/guardian" size="3em" />
+            <BAvatar :to="homeRoute" size="3em" />
         </BCollapse>
     </BNavbar>
 </template>
 
 <script>
 import { useRoute } from 'vue-router'
-import { computed } from 'vue' // Import computed from Vue
+import { computed } from 'vue'
 
 export default {
   setup() {
     const route = useRoute()
 
+    const userId = route.params.id
     const role = computed(() => (route.path.includes('guardian') ? 'guardian' : 'babysitter'))
-    const userId = computed(() => route.params.id || 'defaultUserId')
 
     // Dynamic Home Route based on role and userId
-    const homeRoute = computed(() => `/${userId.value}/${role.value}`)
+    const homeRoute = computed(() => `/${userId}/${role.value}`)
 
     // Dynamic Bookings Route based on role and userId
-    const bookingsRoute = computed(() => `/${userId.value}/${role.value}/bookings`)
+    const bookingsRoute = computed(() => `/${userId}/${role.value}/bookings`)
 
     return {
       homeRoute,
