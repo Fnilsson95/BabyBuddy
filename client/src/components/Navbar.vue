b1d3cec399072a218eaa011b53487bd25ac2f6db
eriklidb <eriklidb@student.chalmers.se>
2024-10-14 15:51:00 +0200

    Change background color and font-weight in navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,27 +1,34 @@
 <template>
-    <BNavbar toggleable="lg" variant="primary" v-b-color-mode="'dark'">
-        <BNavbarBrand :to="homeRoute">BabyBuddy</BNavbarBrand> <!-- Dynamic Home Route -->
-        <BNavbarToggle target="nav-collapse" />
-        <BCollapse id="nav-collapse" is-nav>
-            <BNavbarNav>
-                <BNavItem :to="homeRoute">Home</BNavItem> <!-- Adjusted Home Link -->
-                <BNavItem :to="bookingsRoute">Bookings</BNavItem>
-            </BNavbarNav>
-            <!-- Right aligned nav items -->
-            <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-                <BNavItemDropdown right>
-                    <!-- Using 'button-content' slot -->
-                    <template #button-content>
-                        <em>User</em>
-                    </template>
-                    <BDropdownItem href="#">Profile</BDropdownItem>
-                    <BDropdownItem href="#">My kids</BDropdownItem>
-                    <BDropdownItem to="/login">Sign Out</BDropdownItem>
-                </BNavItemDropdown>
-            </BNavbarNav>
-            <BAvatar :to="homeRoute" size="3em" />
-        </BCollapse>
-    </BNavbar>
+  <BNavbar
+    toggleable="lg"
+    v-b-color-mode="'dark'"
+    style="background-color: #2f4f4f; font-size: 18px; font-weight: 600"
+  >
+    <BNavbarBrand :to="homeRoute">BabyBuddy</BNavbarBrand>
+    <!-- Dynamic Home Route -->
+    <BNavbarToggle target="nav-collapse" />
+    <BCollapse id="nav-collapse" is-nav>
+      <BNavbarNav>
+        <BNavItem :to="homeRoute" :active="$route.path === homeRoute"
+          >Home</BNavItem
+        >
+        <!-- Adjusted Home Link -->
+        <BNavItem :to="bookingsRoute" :active="$route.path === bookingsRoute"
+          >Bookings</BNavItem
+        >
+      </BNavbarNav>
+      <!-- Right aligned nav items -->
+      <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+        <BNavItemDropdown>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <BDropdownItem to="/login">Sign Out</BDropdownItem>
+        </BNavItemDropdown>
+      </BNavbarNav>
+    </BCollapse>
+  </BNavbar>
 </template>
 
 <script>
@@ -33,13 +40,17 @@ export default {
     const route = useRoute()
 
     const userId = route.params.id
-    const role = computed(() => (route.path.includes('guardian') ? 'guardian' : 'babysitter'))
+    const role = computed(() =>
+      route.path.includes('guardian') ? 'guardian' : 'babysitter'
+    )
 
     // Dynamic Home Route based on role and userId
     const homeRoute = computed(() => `/${userId}/${role.value}`)
 
     // Dynamic Bookings Route based on role and userId
-    const bookingsRoute = computed(() => `/${userId}/${role.value}/bookings-history`)
+    const bookingsRoute = computed(
+      () => `/${userId}/${role.value}/bookings-history`
+    )
 
     return {
       homeRoute,
