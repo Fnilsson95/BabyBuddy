71dd72a927de489b02ecfaa2a160ffe890ee2d0a
eriklidb <eriklidb@student.chalmers.se>
2024-10-14 04:38:16 +0200

    Add default background color

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
+    <div id="nav"></div>
     <!-- Render the content of the current page view -->
     <router-view />
   </div>
@@ -14,5 +13,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #f5f5f5;
+  min-height: 100vh;
 }
 </style>
