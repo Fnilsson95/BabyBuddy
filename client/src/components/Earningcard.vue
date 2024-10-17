d2e666ca30bec95b87ccdba596d833492f7482e6
eriklidb <eriklidb@student.chalmers.se>
2024-10-18 00:12:42 +0200

    Render earning card

--- client/src/components/Earningcard.vue
+++ client/src/components/Earningcard.vue
@@ -1,27 +1,48 @@
 <template>
-  <BCard>
-    <div
-      style="
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        gap: 0;
-      "
-    >
-      <p style="font-size: 16px; font-weight: 600; margin: 0">Pending:</p>
-      <p style="font-size: 24px; font-weight: 800; margin: 0">€1000</p>
-    </div>
-  </BCard>
+  <div class="earningCard">
+    <p class="title">
+      {{ title }}
+    </p>
+    <p class="value">{{ value }}$</p>
+  </div>
 </template>
 
-<script>
-import { BCard, BCardText } from 'bootstrap-vue-next'
-
-export default {
-  name: 'earning-card'
-}
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
-<style></style>
+<style>
+.earningCard {
+  width: 150px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0;
+  padding: 6px 0px;
+  border-radius: 4px;
+  border: 1px solid #2f4f4f;
+}
+
+.title {
+  font-size: 14px;
+  font-weight: 600;
+  margin: 0;
+}
+.value {
+  color: #2f4f4f;
+  font-size: 20px;
+  font-weight: 800;
+  margin: 0;
+}
+</style>
